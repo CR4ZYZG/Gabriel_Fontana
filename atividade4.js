1ba8cff3d8fdd618949a035faa335e917990662b
CR4ZYZG <gabrielc.ftn1@gmail.com>
2021-09-24 16:17:35 -0300

    Create atividade4.js

---
+++ atividade4.js
@@ -0,0 +1,9 @@
+var idade = 12
+
+if (idade >= 18) {
+    console.log("Maior de idade")
+} else if (idade > 12 && idade < 18){
+    console.log("Você é um Adolescente")
+} else{
+    console.log("Você é uma criança")
+}
