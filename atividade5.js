861983cabce7a1c6cf4809ccfb36d66b8c99002d
CR4ZYZG <gabrielc.ftn1@gmail.com>
2021-09-24 16:50:07 -0300

    Update atividade5.js

--- atividade5.js
+++ atividade5.js
@@ -1,2 +1,16 @@
-var x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+var list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+var soma = 0
+var media = 0
+var count = 0
 
+for (let i = 0; i < list.length; i++) {
+    if(list[i] % 2 ==0){
+        soma += list[i]
+        count++
+    }
+    
+}
+
+media = soma / count
+
+console.log(media)
