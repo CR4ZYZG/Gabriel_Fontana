d6e82747623fdf8e206a5e05819e8354adbd737e
CR4ZYZG <gabrielc.ftn1@gmail.com>
2021-09-28 13:08:24 -0300

    utilizando varias operações

--- public/bundle.js
+++ public/bundle.js
@@ -1,8 +1,47 @@
 "use strict";
 
-var numbers = [1, 2, 3, 4, 5];
-var newNumbers = numbers.map(function (number) {
-  return number + 1;
+/* const numbers = [1, 2, 3, 4, 5];
+
+/* const newNumbers =  numbers.map(function (number) {
+    return number + 1
+}) */
+
+/* const total = numbers.reduce(function (total, next) {
+    return total + next
+})
+console.log(total)
+
+/* console.log(numbers)
+console.log(newNumbers) */
+//----------------------------------------------
+
+/* const todos = [
+    { text: 'lavar a louça', done: true },
+    { text: 'apanhar o lixo', done : true },
+    { text: 'dobrar as roupas', done : true},
+] */
+
+/* const doneTodos = todos.filter(function (todo) {
+    return todo.done == true
+    //return todo.done == 'dobrar as roupas' 
+})
+
+console.log(doneTodos) */
+
+/* const find = todos.find(function (todo) {
+    return todo.text == 'dobrar as roupas'
+})
+
+console.log(find) */
+
+/* const index = todos.findIndex(function (todo) {
+    return todo.text == 'dobrar as roupas'
+})
+
+console.log(index) */
+//------------------------------------------------
+var cars = ["fusca", "civic", "hb20"];
+var hasFusca = cars.some(function (car) {
+  return car == "bmw";
 });
-console.log(numbers);
-console.log(newNumbers);
+console.log(hasFusca);
--- src/main.js
+++ src/main.js
@@ -1,8 +1,49 @@
-const numbers = [1, 2, 3, 4, 5];
+/* const numbers = [1, 2, 3, 4, 5];
 
-const newNumbers =  numbers.map(function (number) {
+/* const newNumbers =  numbers.map(function (number) {
     return number + 1
+}) */
+
+/* const total = numbers.reduce(function (total, next) {
+    return total + next
+})
+console.log(total)
+
+/* console.log(numbers)
+console.log(newNumbers) */
+
+//----------------------------------------------
+/* const todos = [
+    { text: 'lavar a louça', done: true },
+    { text: 'apanhar o lixo', done : true },
+    { text: 'dobrar as roupas', done : true},
+] */
+
+/* const doneTodos = todos.filter(function (todo) {
+    return todo.done == true
+    //return todo.done == 'dobrar as roupas' 
 })
 
-console.log(numbers)
-console.log(newNumbers)
+console.log(doneTodos) */
+
+/* const find = todos.find(function (todo) {
+    return todo.text == 'dobrar as roupas'
+})
+
+console.log(find) */
+
+/* const index = todos.findIndex(function (todo) {
+    return todo.text == 'dobrar as roupas'
+})
+
+console.log(index) */
+
+//------------------------------------------------
+
+const cars = ["fusca", "civic", "hb20"]
+
+const hasFusca = cars.some(function (car) {
+    return car == "bmw"
+})
+
+console.log(hasFusca)
