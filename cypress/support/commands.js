d45ea6b40070ecda81f6afde4f5b2163b121a846
CR4ZYZG <gabrielc.ftn1@gmail.com>
2021-10-28 16:54:51 -0300

    adição de comando de erro

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,75 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import Faker from 'faker';
+
+Cypress.Commands.overwrite('type',(originalFn,subject,str,options)=>{
+    if(str !==''){
+        return originalFn(subject, str, options)
+    }
+        return subject
+    })
+
+Cypress.Commands.add('logar', (user = 'standard_user') => {
+    const INP_USER  = '[data-test=username]'
+    const INP_PASSWORD = '[data-test=password]'
+    const BTN_LOGIN = '[data-test=login-button]'
+
+    cy.get(INP_USER).type(user)
+    cy.get(INP_PASSWORD).type(`${Cypress.env('password')}`)
+    cy.get(BTN_LOGIN).click()
+    cy.url().should('include', '/inventory.html')
+})
+
+Cypress.Commands.add('contextProdutoNoCarrinho', () => {
+    const NUM_CART = '.shopping_cart_badge'
+    const LIST_PRODUCTS = '.inventory_list'
+    const BTN_ADDTOCART = '[data-test=add-to-cart-sauce-labs-backpack]'
+
+    cy.get(LIST_PRODUCTS).first().should('contain', 'Sauce Labs Backpack')
+    cy.get(BTN_ADDTOCART).click()
+    cy.get(NUM_CART).should('contain', 1)
+})
+
+Cypress.Commands.add('acessarCarrinho', () => {
+    const BTN_CART = '.shopping_cart_link'
+
+    cy.get(BTN_CART).click()
+    cy.url().should('include', '/cart.html')
+})
+
+Cypress.Commands.add('checkoutWrongInfo', () => {
+    const BTN_CHECKOUT = '[data-test=checkout]'
+    const INP_FIRSTNAME = '[data-test=firstName]'
+    const INP_LASTNAME = '[data-test=lastName]' 
+    //isso não vai receber nenhum dado para dar erro
+    const INP_ZIPCODE = '[data-test=postalCode]'
+    const BTN_CHECKOUT_CONTINUE = '[data-test=continue]'
+
+    cy.get(BTN_CHECKOUT).click()
+    cy.url().should('include', 'checkout-step-one.html')
+    cy.get(INP_FIRSTNAME).type('')
+    cy.get(INP_LASTNAME).type('')
+    cy.get(INP_ZIPCODE).type(Faker.company.companyName())
+    cy.get(BTN_CHECKOUT_CONTINUE).click()
+    cy.url().should('include', 'checkout-step-one.html')
+})
+
+Cypress.Commands.add('checkoutCorrectInfo', () => {
+    const BTN_CHECKOUT = '[data-test=checkout]'
+    const INP_FIRSTNAME = '[data-test=firstName]'
+    const INP_LASTNAME = '[data-test=lastName]'
+    const INP_ZIPCODE = '[data-test=postalCode]'
+    const BTN_CHECKOUT_CONTINUE = '[data-test=continue]'
+    const BTN_CHECKOUT_FINISH = '[data-test=finish]'
+
+    cy.get(BTN_CHECKOUT).click()
+    cy.url().should('include', 'checkout-step-one.html')
+    cy.get(INP_FIRSTNAME).type(Faker.name.firstName())
+    cy.get(INP_LASTNAME).type(Faker.name.lastName())
+    cy.get(INP_ZIPCODE).type(Faker.address.zipCode())
+    cy.get(BTN_CHECKOUT_CONTINUE).click()
+    cy.url().should('include', 'checkout-step-two.html')
+    cy.get(BTN_CHECKOUT_FINISH).click()
+    cy.url().should('include', 'checkout-complete.html')
+})
+
+
+
